7104ce109f149fbae9730fff12a5a8dfa2ad15db
Satyam Kumar <satyamkumar629301@gmail.com>
2025-12-27 13:43:32 +0530

    Redux boilderplate

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,19 @@
+
+
+
+/* 
+
+STEPS for State Management
+
+Submit Action
+Handle Action in it's reducer
+Register here => Reducer
+
+*/
+
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+    reducer:{}
+})
