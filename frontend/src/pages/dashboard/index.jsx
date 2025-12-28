30ab68fffd6e05dfad3dd937ee051f8ebeba172e
Satyam Kumar <satyamkumar629301@gmail.com>
2025-12-28 22:53:36 +0530

    complete authentication

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 
 function dashboard() {
 
-    return ( <h1>dashboard</h1> );
+    const router = useRouter(); 
+    useEffect(() => {
+        if(localStorage.getItem("token") === null ) {
+            router.push("/login")
+        }
+    })
+
+    return (
+        <h1>dashboard</h1>
+    );
 }
 
 export default dashboard;
