8e10a0b6495622bd5519bdb78aec8df15f43c615
Satyam Kumar <satyamkumar629301@gmail.com>
2026-01-04 00:35:45 +0530

    Download resume functionality

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -84,13 +84,27 @@ function ViewProfilePage({userProfile}) {
 
 
 
-                                {isCurrentUserInConnection ?
-                                    <button className={styles.connectedButton}>{isConnectionNull ? "Pending" : "Connected"}</button>
-                                    :
-                                    <button onClick={() => {
-                                        dispatch(sendConnectionRequest({ token : localStorage.getItem("token"), connectionId : userProfile.userId._id })) 
-                                    }} className={styles.connectBtn}>Connect</button>
-                                }
+                                <div style={{display:"flex", alignItems:"center", gap:"1.2rem"}}>
+                                    {isCurrentUserInConnection ?
+                                        <button className={styles.connectedButton}>{isConnectionNull ? "Pending" : "Connected"}</button>
+                                        :
+                                        <button onClick={() => {
+                                            dispatch(sendConnectionRequest({ token : localStorage.getItem("token"), connectionId : userProfile.userId._id }))
+                                        }} className={styles.connectBtn}>Connect</button>
+                                    }
+
+                                    <div onClick={async() => {
+                                        const response = await clientServer.get(`/user/download_resume?id=${userProfile.userId._id}`);
+                                        window.open(`${BASE_URL}/${response.data.message}`, "_blank")
+                                    }} style={{cursor:"pointer"}}>
+
+                                        <svg style={{width:"1.2em"}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                        </svg>
+
+                                    </div>
+
+                                </div>
 
                                 <div>
                                     <p>{userProfile.bio}</p>
@@ -125,6 +139,25 @@ function ViewProfilePage({userProfile}) {
                                 }
                             </div>
                             
+                        </div>
+                        
+                    </div>
+
+
+                    <div className={styles.workHistory}>
+                        <h4>Past Work</h4>
+
+                        <div className={styles.workHistoryContainer}>
+                            {
+                                userProfile.pastWork.map((work, index) => {
+                                    return (
+                                        <div key={index} className={styles.workHistoryCard}>
+                                            <p style={{fontWeight:"bold", display:"flex", alignItems:"center", gap:"0.8rem"}}>{work.company} - {work.position}</p>
+                                            <p>{work.years}</p>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                         
                     </div>
